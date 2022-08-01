470224ce60d9d29446f8c28ea199d1ee28ccb30f
jungkeunmo <abc59684495@gmail.com>
2022-08-01 20:37:17 +0900

    obj_p

---
+++ models/borad.js
@@ -0,0 +1,18 @@
+class Board {
+
+    id;
+    title;
+    content;
+    hit;
+    createdAt;
+
+    getId() {
+        return this.id;
+    };
+
+    setId(value) {
+        this.id = value;
+    };
+
+    
+}
